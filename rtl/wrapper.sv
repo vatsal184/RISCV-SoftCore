e03aaee89e31293217119f18a09bce4af3bd42ac
vatsal184 <43042897+vatsal184@users.noreply.github.com>
2019-04-08 09:45:41 -0400

    Update wrapper.sv

--- rtl/wrapper.sv
+++ rtl/wrapper.sv
@@ -1,28 +1,25 @@
 `include "csr.sv"
+`include "IM.sv"
 `include "DM.sv"
-`include "core.sv"
 
 
-module riscv_wrapper(  
+module riscv_wrapper(
+	input clk,
+	input reset,
+	output trap
+);
   
-      input clk,
-      input reset,
-  output trap,
-  output [31:0]pc,
-  input [31:0]instr);
-
-
-  wire [31:0] csr_wr_data, csr_rd_data, m_addr, m_wr_dat;
-  reg [31:0]m_rd_dat;
+  wire [31:0] pc , csr_wr_data, csr_rd_data, m_addr, m_wr_dat;
+  reg [31:0]instr ,m_rd_dat;
   wire [11:0]csr_rd_addr;
   
-  riscv_core 			cpu(
-    		.clk(clk),.reset(reset),
-                 
-    		.pc(pc),.instr_in(instr),
-    
-    		.trap(trap),
-            
+  riscv_core cpu(
+            .clk(clk),.reset(reset),
+
+            .pc(pc),.instr(instr),
+
+            .trap(trap),
+                        
             .csr(csr),
             .csr_rd_addr(csr_rd_addr),
             .csr_wr_data(csr_wr_data),
@@ -35,12 +32,12 @@ module riscv_wrapper(
             .m_rd_dat(m_rd_dat)
            );
 
-/* IM ins_mem(.clk(clk),
+ IM ins_mem(.clk(clk),
             .reset(reset),
-            .pc(pc + INS_BASE_ADDR),
+            .pc(pc),
             .instr(instr)
            );
-*/	
+
   
 cs_reg csre(.clk(clk),
             .reset(reset),
@@ -59,14 +56,14 @@ memory mem(.clk(clk),
              .m_rd_dat(m_rd_dat)
             );
   
-  /*
+/*  
   always @(pc) begin
   $display("PC          = %d  ",pc);
   $display("Reset       = %d  ",reset);
   $display("Instruction = 0x%h  ",instr);
-//  $display("ALU output  = %d  ",m_addr); 
+  $display("ALU output  = %d  ",m_addr); 
 //  if (MemWrite) $display("Data 0x%h written to address %d",m_wr_dat, m_addr);
     $display("---------------------------------------------------------------");
-end*/
-  
+end
+  */
   endmodule
