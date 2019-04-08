518c9586d2ef985c0c4c4d3b7d92293cae4b9fa2
vatsal184 <43042897+vatsal184@users.noreply.github.com>
2019-04-08 10:33:42 -0400

    Update IM.sv

--- rtl/IM.sv
+++ rtl/IM.sv
@@ -1,21 +1,18 @@
-
 module IM(
     input clk,
   	input reset,
     input [31:0]pc,
-    output reg [31:0]instr
+    output [31:0]instr
     );
     
         
   reg [31:0] IM [1023:0];
+  wire [31:0] next_pc;
   
 initial begin 
   $readmemh("ins_mem.txt",IM);
-
 end
-  always @(posedge clk) begin
-    if (reset)	instr = IM[pc >> 2];
-    else instr = 32'b0;
-  end
+  
+    assign instr = reset ? IM[pc >> 2] : 32'b0;
 
 endmodule
